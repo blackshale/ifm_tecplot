1750e1d2a16a1fc373804d02d61fcac3ef03e709
blackshale <jjabssil1127@gamil.com>
2021-01-21 19:56:37 +0900

    initial commit files copied

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ifm_tecplot
 FEFLOW ifm module making output files in Tecplot format
+
+1/21/2020 test.. files were transferred. 
---
+++ ifmtecplot_v1.cxx
@@ -0,0 +1,146 @@
+#include <ifm/module.h>
+#include <ifm/graphic.h>
+#include <ifm/document.h>
+#include <ifm/archive.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/* --- IFMREG_BEGIN --- */
+/*  -- Do not edit! --  */
+
+static void PreSimulation (IfmDocument);
+
+static const char szDesc[] = 
+  "Write modeling results in Tecplot format";
+
+#ifdef __cplusplus
+extern "C"
+#endif /* __cplusplus */
+#ifdef WIN32
+__declspec(dllexport)
+#endif /* WIN32 */
+
+IfmResult RegisterModule(IfmModule pMod)
+{
+  if (IfmModuleVersion (pMod) < IFM_CURRENT_MODULE_VERSION)
+    return False;
+  IfmRegisterModule (pMod, "SIMULATION", "IFMTECPLOT_V1", "Ifmtecplot_v1", 0x1000);
+  IfmSetDescriptionString (pMod, szDesc);
+  IfmSetCopyrightPath (pMod, "ifmtecplot_v1.txt");
+  IfmSetHtmlPage (pMod, "ifmtecplot_v1.htm");
+  IfmSetPrimarySource (pMod, "ifmtecplot_v1.cxx");
+  IfmRegisterProc (pMod, "PreSimulation", 1, (IfmProc)PreSimulation);
+  return True;
+}
+/* --- IFMREG_END --- */
+
+static void PreSimulation (IfmDocument pDoc)
+{
+  /* 
+   * TODO: Add your own code here ...
+   */
+
+	/* Declaration */
+	int i,j;
+
+	int NumNodes;
+	int NumElements;
+
+	FILE* fp = NULL;
+	char tecfile[200] = "";
+
+
+	/* Assignment */
+		//get number of nodes and elements
+	NumNodes = IfmGetNumberOfNodes(pDoc);
+	NumElements = IfmGetNumberOfElements(pDoc);
+		//get nodewise properties
+	double xcoord[NumNodes],ycoord[NumNodes],zcoord[NumNodes];
+
+	for(i=0; i<NumNodes; i++){
+		xcoord[i] = IfmGetX(pDoc,i);
+		ycoord[i] = IfmGetY(pDoc,i);
+		zcoord[i] = IfmGetZ(pDoc,i);
+	}
+
+
+		//get elementwise properties
+	int elind[NumElements][8];
+
+	for(i=0; i<NumElements; i++){
+		for (j=0;j<6;j++){
+			elind[i][j] = IfmGetNode(pDoc,i,j)+1;
+		}
+		elind[i][7]=elind[i][5]; //adjustment prism--> block
+		elind[i][6]=elind[i][5];
+		elind[i][5]=elind[i][4];
+		elind[i][4]=elind[i][3];
+		elind[i][3]=elind[i][2];
+	}
+
+
+		//get directory name and assign file name
+        strcat(tecfile, IfmGetFileDirectory(pDoc,IfmGetProblemPath(pDoc)));
+	strcat(tecfile, "tecgrid.dat");
+
+
+	/* Show in the info box */
+	IfmInfo(pDoc, "Number of Nodes = %li\n", NumNodes);
+	IfmInfo(pDoc, "Number of Elements = %li\n", NumElements);
+
+
+	/* Open a file */
+	fp = fopen (tecfile, "wt");
+
+	/* Tecplot header */
+	fprintf(fp, "TITLE = \"FEFLOW GRID\"\n");
+	fprintf(fp, "VARIABLES = \"X\",\"Y\",\"Z\"\n");	
+		
+	/* Tecplot zone */
+
+	for (i=1; i<2; i++){
+		fprintf(fp, "ZONE T = \"ZONE  %2i\",\n",i);
+		fprintf(fp, "\tN = %10i, E = %10i,\n", NumNodes,NumElements); 	
+		fprintf(fp, "\tZONETYPE= FEBRICK,\n");
+		fprintf(fp, "\tDATAPACKING= BLOCK,\n");
+		fprintf(fp, "\tSOLUTIONTIME= 0,\n");
+		fprintf(fp, "\tVARLOCATION= ([1-3]=NODAL)\n");
+	}
+
+	/* Tecplot data blocks */
+		//x-coordinates
+	for (i=0; i<NumNodes; i++){
+			fprintf(fp, "%.10e\n", xcoord[i]);
+	}	
+		//y-coordinates
+	for (i=0; i<NumNodes; i++){
+			fprintf(fp, "%.10e\n", ycoord[i]);
+	}	
+		//z-coordinates
+	for (i=0; i<NumNodes; i++){
+			fprintf(fp, "%.10e\n", zcoord[i]);
+	}
+	
+		//element indexes
+	for (i=0; i<NumElements; i++){
+		for(j=0;j<8;j++){
+			fprintf(fp,"%10i ",elind[i][j]);
+		}
+		fprintf(fp, "\n");
+	}
+
+	fclose(fp);
+
+
+	IfmInfo(pDoc, "Done:PreSimulation()");
+
+
+
+
+
+
+
+
+ 
+
+}
