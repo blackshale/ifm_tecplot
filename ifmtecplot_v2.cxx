4a546505c4c41904cc5c763aa2b8325f3e47d74d
blackshale <jjabssil1127@gamil.com>
2021-01-27 19:14:39 +0900

    basics are almost done.

--- ifmtecplot_v2.cxx
+++ ifmtecplot_v2.cxx
@@ -4,6 +4,11 @@
 #include <ifm/archive.h>
 #include <stdio.h>
 #include <stdlib.h>
+//added by BJ for file contol
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
 
 /* --- IFMREG_BEGIN --- */
 /*  -- Do not edit! --  */
@@ -48,7 +53,9 @@ static void PreSimulation (IfmDocument pDoc)
  	int NumElements;
 
  	FILE* fp = NULL;
- 	char tecfile[200] = "";
+ 	char tecdir[200] = "";
+  char tecfile[200] = "";
+  char rmfile[200] = "";
 
 
  	/* Assignment */
@@ -86,9 +93,23 @@ static void PreSimulation (IfmDocument pDoc)
 
 
  		//get directory name and assign file name
-  strcat(tecfile, IfmGetFileDirectory(pDoc,IfmGetProblemPath(pDoc)));
- 	strcat(tecfile, "tecgrid.dat");
+  strcat(tecdir, IfmGetFileDirectory(pDoc,IfmGetProblemPath(pDoc)));
+  strcat(tecdir,"tecresults/");
 
+  struct stat st = {0};
+  if (stat(tecdir, &st) == -1) {
+      mkdir(tecdir, 0700); // check if tecresults folder exists, if not create it
+  }
+  else {
+      strcat(rmfile,"rm ");
+      strcat(rmfile,tecdir);
+      strcat(rmfile,"*.*");
+      system(rmfile); // remove all files in directory if exists.
+      //IfmInfo(pDoc, rmfile);
+  }
+
+  strcat(tecfile,tecdir);
+  strcat(tecfile, "tecgrid.dat");
 
  	/* Show in the info box */
   IfmInfo(pDoc, "IFM_Tecplot_v2.0");
@@ -156,7 +177,6 @@ static void PostTimeStep (IfmDocument pDoc)
    Files will be made at each time step.
    */
 
-
    /* Declaration */
  	int i,j;
 
@@ -164,13 +184,15 @@ static void PostTimeStep (IfmDocument pDoc)
  	int NumElements;
 
  	FILE* fp = NULL;
+  char tecdir[200] = "";
  	char tecfile[200] = "";
+  char temp_string[30] = "";
+  char VariableList[300] = "";
 
 
  	/* Assignment */
     // get timestep information
   double AbsoluteSimulationTime;
-
   AbsoluteSimulationTime = IfmGetAbsoluteSimulationTime(pDoc);
 
 
@@ -210,11 +232,13 @@ static void PostTimeStep (IfmDocument pDoc)
 
 
  		//get directory name and assign file name
-  strcat(tecfile, IfmGetFileDirectory(pDoc,IfmGetProblemPath(pDoc)));
- 	strcat(tecfile, "tecresults-");
-  snprintf(tecfile,10,"%.10e",AbsoluteSimulationTime);
-  strcat(tecfile, ".dat");
-  IfmInfo(pDoc, tecfile);
+  strcat(tecdir, IfmGetFileDirectory(pDoc,IfmGetProblemPath(pDoc)));
+ 	strcat(tecdir, "tecresults/");
+
+  snprintf(temp_string,22,"t%.10e.dat",AbsoluteSimulationTime);
+  strcat(tecfile, tecdir);
+  strcat(tecfile, temp_string);
+  //IfmInfo(pDoc, tecfile);
 
  	/* Show in the info box */
   IfmInfo(pDoc, "Printing results in Tecplot format (t = %.10e)",AbsoluteSimulationTime);
@@ -224,16 +248,41 @@ static void PostTimeStep (IfmDocument pDoc)
 
  	/* Tecplot header */
  	fprintf(fp, "TITLE = \"FEFLOW GRID\"\n");
- 	fprintf(fp, "VARIABLES = \"X\",\"Y\",\"Z\",\"MP\"\n");
+
+  /* get problem class and type */
+  // IfmGetProblemClass : -1: illegal, 0:flow, 1:flow+mass, 2:flow+heat, 3:flow+mass+heat
+  // IfmGetProblemType: 0:saturated, 1:unsaturated
+  int ProblemClass, ProblemType;
+  ProblemClass = IfmGetProblemClass(pDoc);
+  ProblemType = IfmGetProblemType(pDoc);
+
+  strcat(VariableList, "VARIABLES = \"X\",\"Y\",\"Z\",\"MP\"");
+
+  if(ProblemClass >= 0){
+    strcat(VariableList, ",\"Head\",\"Pressure\",\"DarcyVelocity\",\"X-Velocity\",\"Y-Velocity\",\"Z-Velocity\"");
+    if(ProblemType == 1){
+      strcat(VariableList, ",\"Saturation\"");
+    }
+  }
+  if(ProblemClass == 1 || ProblemClass == 3){
+    strcat(VariableList, ",\"Concentration\"");
+  }
+  if(ProblemClass >=2){
+    strcat(VariableList, ",\"Temperature\"");
+  }
+  strcat(VariableList, "\n");
+
+
+ 	fprintf(fp, VariableList);
 
  	/* more Tecplot header  */
 
-	fprintf(fp, "ZONE T = \"ZONE  1 at %.10e\",\n",AbsoluteSimulationTime);
+	fprintf(fp, "ZONE T = \"t= %.10e\",\n",AbsoluteSimulationTime);
 	fprintf(fp, "\tN = %10i, E = %10i,\n", NumNodes,NumElements);
 	fprintf(fp, "\tZONETYPE= FEBRICK,\n");
 	fprintf(fp, "\tDATAPACKING= BLOCK,\n");
-	fprintf(fp, "\tSOLUTIONTIME= 0,\n");
-	fprintf(fp, "\tVARLOCATION= ([1-3]=NODAL,[4]=CELLCENTERED)\n");
+	fprintf(fp, "\tSOLUTIONTIME= %.10e,\n",AbsoluteSimulationTime);
+	fprintf(fp, "\tVARLOCATION= ([1-3,5-13]=NODAL,[4]=CELLCENTERED)\n");
 
 
  	/* Tecplot data blocks */
@@ -249,13 +298,69 @@ static void PostTimeStep (IfmDocument pDoc)
  	for (i=0; i<NumNodes; i++){
  			fprintf(fp, "%.10e\n", zcoord[i]);
  	}
-
    //material property indexes
    for (i=0; i<NumElements; i++){
       fprintf(fp, "%3i\n", mp[i]);
+  }
+
+  // Flow only (+ saturation)
+  if (ProblemClass >= 0){
+     // hydraulic head
+     for (i=0; i<NumNodes; i++){
+        fprintf(fp, "%.10e\n", IfmGetResultsFlowHeadValue(pDoc,i));
+     }
+     // pressure
+     for (i=0; i<NumNodes; i++){
+        fprintf(fp, "%.10e\n", IfmGetResultsFlowPressureValue(pDoc,i));
+     }
+     // darcy velocity
+     for (i=0; i<NumNodes; i++){
+        fprintf(fp, "%.10e\n", IfmGetResultsVelocityNormValue(pDoc,i));
+     }
+
+     // x velocity
+     for (i=0; i<NumNodes; i++){
+        fprintf(fp, "%.10e\n", IfmGetResultsXVelocityValue(pDoc,i));
+     }
+
+     // y velocity
+     for (i=0; i<NumNodes; i++){
+        fprintf(fp, "%.10e\n", IfmGetResultsYVelocityValue(pDoc,i));
+     }
+
+     // z velocity
+     for (i=0; i<NumNodes; i++){
+        fprintf(fp, "%.10e\n", IfmGetResultsZVelocityValue(pDoc,i));
+     }
+
+     if (ProblemType == 1){
+       // saturation
+       for (i=0; i<NumNodes; i++){
+          fprintf(fp, "%.10e\n", IfmGetResultsFlowSaturationValue(pDoc,i));
+       }
+     }
+  }
+
+   // Flow + Mass
+  if(ProblemClass == 1 || ProblemClass ==3){
+     // concentration
+     for (i=0; i<NumNodes; i++){
+        fprintf(fp, "%.10e\n", IfmGetResultsTransportMassValue(pDoc,i));
+     }
+  }
+
+  // Flow + Heat
+  if(ProblemClass >= 2){
+     // temperature
+     for (i=0; i<NumNodes; i++){
+        fprintf(fp, "%.10e\n", IfmGetResultsTransportHeatValue(pDoc,i));
+     }
    }
 
- 		//element indexes
+
+
+
+	//MUST-HAVE:  element indexes
  	for (i=0; i<NumElements; i++){
  		for(j=0;j<8;j++){
  			fprintf(fp,"%10i ",elind[i][j]);
@@ -263,11 +368,13 @@ static void PostTimeStep (IfmDocument pDoc)
  		fprintf(fp, "\n");
  	}
 
+
+
  	fclose(fp);
 
 
   // end of PreSimulation
- 	IfmInfo(pDoc, "Done:PreSimulation()");
+ 	//IfmInfo(pDoc, "Done:PreSimulation()");
 
 
 }
